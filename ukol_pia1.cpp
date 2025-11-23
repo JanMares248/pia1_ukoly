94a8f815b3f41ad1fc5ca40682c4cfb607507f8e
JanMares248 <125661185+JanMares248@users.noreply.github.com>
2025-11-23 16:52:41 +0100

    Ukol kruh

---
+++ ukol_pia1.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using parametr = float;
+struct Kruh
+{
+    Kruh() = default;
+    Kruh(parametr r)
+    {
+        radius = r;
+    }
+
+    void setRadius(parametr r)
+    {
+        radius = r;
+        
+    }
+    void vypisUdaje()
+    {
+        std::cout << "Radius je:" << radius << std::endl;
+        std::cout << "Plocha kruhu:" << spocitejPlochu() << std::endl;
+        std::cout << "Obvod kruhu:" << spocitejObvod() << std::endl;
+    }
+    parametr spocitejPlochu()
+    {
+        return  pi * pow(2, radius);
+        
+    }
+    parametr spocitejObvod()
+    {
+        return (parametr)2 * pi * radius;
+    }
+private:
+
+    parametr radius;
+    const parametr pi = (parametr)3.14;
+
+};
+
+int main()
+{
+    float prvniRadius = 2;
+    Kruh prvniKruh;
+    prvniKruh.setRadius(prvniRadius);
+
+    float druhyRaidus = 4;
+    Kruh druhyKruh(druhyRaidus);
+    std::cout << "Plocha kruhu:" << prvniKruh.spocitejPlochu() << std::endl;
+    std::cout << "Obvod kruhu:" << prvniKruh.spocitejObvod() << std::endl;
+
+    druhyKruh.vypisUdaje();
+
+
+    return 0;
+}
